24aeaf19798dd5df19e1089fc039cd98124e9d03
RdeRausseo <amrausseo20@gmail.com>
2025-01-16 18:09:57 +0000

    conectando base de datos

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,35 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
+
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link to="/singup" className="nav-link active" aria-current="page" href="#">
+								Sing up
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/login" className="nav-link" href="#">
+								Login
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
-	);
+	)
 };
